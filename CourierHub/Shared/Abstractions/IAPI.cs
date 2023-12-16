1cacab109f3760865a617582b08c3773ce2a7044
Bakaczor <bartosz.kaczorowski@protonmail.com>
2023-12-16 13:25:04 +0100

    uwagi dla Kamila

---
+++ CourierHub/Shared/Abstractions/IAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierHub.Shared.Abstractions {
+    internal interface IAPI {
+    }
+}
+namespace CourierHub.Shared.Abstractions {
+    public interface IWebAPI {
+        /* tutaj musisz Kamilu dać 
+         * POST /inquires {} -> /inquires/id
+         * GET /offers/id -> {}
+         * albo połączyć te dwa w jedno
+         * oraz
+         * POST /orders {} -> orders/id
+         * PATCH /orders/id {}
+         * GET /orders/id -> {}
+         */
+    }
+}
