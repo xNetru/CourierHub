5882c53bd19ed3f0abe7efbb7c404670a3ab4af3
Maksymilian Czapski <maks.czapski@gmail.com>
2023-12-18 17:21:35 +0100

    offer submit layout + redirection from offerList

--- CourierHub/Shared/ApiModels/ApiOrder.cs
+++ CourierHub/Shared/ApiModels/ApiOrder.cs
@@ -1,15 +1,24 @@
-﻿namespace CourierHub.Shared.ApiModels {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourierHub.Shared.ApiModels {
     public class ApiOrder {
         public int ApiInquire { get; set; }
 
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Email jest wymagany")]
+        [EmailAddress(ErrorMessage = "Niepoprawny format maila")]
         public string ClientEmail { get; set; } = null!;
 
+        [Required(ErrorMessage = "Imię jest wymagane")]
+        [RegularExpression(@"^[A-Z][a-z]*$", ErrorMessage = "Imię musi się zaczynać od duzej litery")]
         public string ClientName { get; set; } = null!;
 
+        [Required(ErrorMessage = "Nazwisko jest wymagane")]
+        [RegularExpression(@"^[A-Z][a-z]*$", ErrorMessage = "Nazwisko musi się zaczynać od duzej litery")]
         public string ClientSurname { get; set; } = null!;
 
+        [Phone]
         public string ClientPhone { get; set; } = null!;
 
         public string ClientCompany { get; set; } = null!;
