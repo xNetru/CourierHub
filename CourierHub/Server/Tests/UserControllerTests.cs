ea52d89e762019c1c96c3533637c0e344d910129
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-01-07 20:21:09 +0100

    testing user controller

---
+++ CourierHub/Server/Tests/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Castle.Core.Resource;
+using CourierHub.Shared.Controllers;
+using CourierHub.Shared.Data;
+using CourierHub.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace CourierHub.Server.Tests; 
+public class UserControllerTests {
+    private readonly UserController _controller;
+
+    public UserControllerTest() {
+        var mockSet = new Mock<DbSet<User>>();
+        var users = new List<User>
+        {
+            new() { Id = 1, Email = "januszkowalski@gmail.com", Name = "Janusz", Surname = "Kowalski", Type = 1 },
+        }.AsQueryable();
+        mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(users.Provider);
+        mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(users.Expression);
+        mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(users.ElementType);
+        mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(users.GetEnumerator());
+
+        var mockContext = new Mock<CourierHubDbContext>();
+        mockContext.Setup(c => c.Users).Returns(mockSet.Object);
+        _controller = new UserController(mockContext.Object);
+    }
+
+    [Fact]
+    public async Task Head_ShouldReturn200_WhenUserWithEmailExists() {
+
+    }
+}
