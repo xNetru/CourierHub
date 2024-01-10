5106584e3e05b0f6e693805c6f8b77fde383b7bc
xNetru <kamilbbank@gmail.com>
2024-01-10 22:07:07 +0100

    Working on Szymo Api

--- CourierHub/Server/Data/SzymoHubApi.cs
+++ CourierHub/Server/Data/SzymoHubApi.cs
@@ -1,6 +1,8 @@
-﻿using CourierHub.Shared.Abstractions;
+﻿using CourierHub.CourierHubApiModels;
+using CourierHub.Shared.Abstractions;
 using CourierHub.Shared.ApiModels;
 using CourierHub.Shared.Enums;
+using CourierHub.Shared.SzymoApiModels;
 
 namespace CourierHub.Server.Data;
 public class SzymoHubApi : IWebApi {
@@ -14,11 +16,57 @@ public class SzymoHubApi : IWebApi {
     }
 
     public async Task<(StatusType?, int)> GetOrderStatus(string code) {
+    
+
         Console.WriteLine("GetOrderStatus was invoked in SzymoHubApi.");
         return (null, 400);
     }
 
     public async Task<(ApiOffer?, int)> PostInquireGetOffer(ApiInquire inquire) {
+        var source = new SzymoAddress(
+            inquire.Source.Number,
+            inquire.Source.Flat,
+            inquire.Source.Street,
+            inquire.Source.City,
+            inquire.Source.PostalCode,
+            "Poland");
+
+        var destination = new SzymoAddress(
+            inquire.Destination.Number,
+            inquire.Destination.Flat,
+            inquire.Destination.Street,
+            inquire.Destination.City,
+            inquire.Destination.PostalCode,
+            "Poland");
+
+        var dimensions = new SzymoDimensions(
+            inquire.Width / 100.0f,
+            inquire.Length / 100.0f,
+            inquire.Depth / 100.0f,
+            "Meters");
+
+        var szymoInquiry = new SzymoInquiry(
+            dimensions,
+            "Pln",
+            inquire.Mass / 1000.0f,
+            "Kilograms",
+            source,
+            destination,
+            inquire.SourceDate,
+            inquire.DestinationDate,
+            inquire.IsWeekend,
+            inquire.Priority switch
+            {
+                (int)PriorityType.High => "High",
+                (int)PriorityType.Low => "Low",
+                (int)PriorityType.Medium => "Medium",
+                _ => "Unknown"
+            }, 
+            true,
+            inquire.IsCompany);
+
+
+
         Console.WriteLine("PostInquireGetOffer was invoked in SzymoHubApi.");
         return (null, 400);
     }
@@ -32,4 +80,57 @@ public class SzymoHubApi : IWebApi {
         Console.WriteLine("PutOrderWithrawal was invoked in SzymoHubApi.");
         return 400;
     }
+
+    static async Task<string?> GetAccessToken(string clientId, string clientSecret, string tokenEndpoint)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var credentials = $"{clientId}:{clientSecret}";
+            var base64Credentials = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(credentials));
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
+
+            var formData = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials"),
+            });
+
+            var response = await client.PostAsync(tokenEndpoint, formData);
+            if(response.IsSuccessStatusCode)
+            {
+                var tokenResponse = await response.Content.ReadFromJsonAsync<AccessTokenResponse>();
+                if(tokenResponse != null) 
+                {
+                    return tokenResponse.access_token;
+                } 
+                else
+                {
+                    return null;
+                }
+            } 
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    static async Task MakeApiRequest(string apiUrl, string accessToken)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+            var response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+    }
+}
+
+class AccessTokenResponse
+{
+    public string? access_token { get; set; }
 }
---
+++ CourierHub/Shared/SzymoApiModels/SzymoAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierHub.Shared.SzymoApiModels
+{
+    public record SzymoAddress(
+        string? houseNumber,
+        string? apartmentNumber,
+        string? street,
+        string? city,
+        string? zipCode, 
+        string? country);
+}
