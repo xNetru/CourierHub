4aeea9c86dfad29d37ca2dcc4dfbd157c74691d4
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-01-13 22:47:51 +0100

    sth

--- CourierHub.Test/ApiControllerTest.cs
+++ CourierHub.Test/ApiControllerTest.cs
@@ -1,27 +1,90 @@
-﻿using CourierHub.Server.Api;
+﻿using CourierHub.Cloud;
+using CourierHub.Server.Api;
 using CourierHub.Server.Containers;
 using CourierHub.Shared.ApiModels;
+using CourierHub.Shared.Controllers;
 using CourierHub.Shared.Data;
-using CourierHub.Shared.Enums;
 using CourierHub.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Moq;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CourierHub.Shared.Controllers;
-[ApiController]
-[Route("[controller]")]
-public class ApiController : ControllerBase {
-    private readonly CourierHubDbContext _context;
-    private readonly IList<(List<string>, int)> _inquireCodes;
-    private readonly IList<IWebApi> _webApis;
+namespace CourierHub.Test {
+    public class ApiControllerTest {
+        private readonly ApiController _controller;
 
-    public ApiController(CourierHubDbContext context, WebApiContainer apiContainer, InquireCodeContainer inquireContainer) {
-        _context = context;
-        _inquireCodes = inquireContainer.InquireCodes;
-        _webApis = apiContainer.WebApis;
-    }
+        public ApiControllerTest() {
+            var inqCodeMock = new Mock<InquireCodeContainer>();
+            inqCodeMock.Setup(c => c.InquireCodes).Returns(new List<(List<string>, int)> {
+                (new List<string> { "0123" }, 1) 
+            });
 
-    // POST: <ApiController>/inquire/{...}
+            var apiMock = new Mock<IWebApi>();
+            var apiOffer = new ApiOffer {
+                Code = "0123",
+                Price = 1000
+            };
+            apiMock.Setup(c => c.PostInquireGetOffer(It.IsAny<ApiInquire>())).Returns(Task.FromResult<(ApiOffer?, int)>((apiOffer, 201)));
+            apiMock.Setup(c => c.PostOrder(It.IsAny<ApiOrder>())).Returns(Task.FromResult<(int, string?)>((201, null)));
+            apiMock.Setup(c => c.PutOrderWithrawal(It.IsAny<string>())).Returns(Task.FromResult(201));
+
+            var apisMock = new Mock<WebApiContainer>();
+            apisMock.Setup(c => c.WebApis).Returns(new List<IWebApi> { apiMock.Object });
+
+            var mockContext = new Mock<CourierHubDbContext>();
+
+            var address = new Address() { Id = 1, City = "Warszawa" };
+            IList<Address> addresses = new List<Address> { address };
+            mockContext.Setup(c => c.Addresses).ReturnsDbSet(addresses);
+
+            IList<User> users = new List<User> {
+                new() { Id = 1, Email = "januszkowalski@gmail.com", Name = "Janusz", Surname = "Kowalski", Type = 0 },
+            };
+
+            IList<Inquire> inquires = new List<Inquire> {
+                new() { Id = 1, Code = "0123", Datetime = DateTime.Now, Source = address, Destination = address },
+            };
+            mockContext.Setup(c => c.Inquires).ReturnsDbSet(inquires);
+
+            IList<Service> services = new List<Service> {
+            new() { Id = 1, Name = "TestService", ApiKey = "666 - the Number of the Beast" }
+            };
+            mockContext.Setup(c => c.Services).ReturnsDbSet(services);
+
+            IList<Order> orders = new List<Order> {
+            new() { Id = 1, InquireId = 1, Inquire = inquires[0], ClientAddressId = 1, ServiceId = 1, Service = services[0],
+                ClientName = "Janusz", ClientSurname = "Kowalski", ClientEmail =  "januszkowalski@gmail.com",
+            }
+            };
+            mockContext.Setup(c => c.Orders).ReturnsDbSet(orders);
+
+            _controller = new ApiController(mockContext.Object, apisMock.Object, inqCodeMock.Object);
+        }
+
+        [Fact]
+        public async Task PostInquireGetOffers_ShouldReturnOffer_WhenClientExists() {
+            // Arrange
+            var inquire = new ApiInquire {
+                Datetime = DateTime.Now,
+                Source = new ApiAddress { City = "Warszawa" },
+                Destination = new ApiAddress { City = "Kraków" }
+            };
+            // Act
+            var result = await _controller.PostInquireGetOffers(inquire);
+            // Assert
+            OkObjectResult status = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, status.StatusCode);
+        }
+
+        /*
+         * \
+         * // POST: <ApiController>/inquire/{...}
     [HttpPost("inquire")]
     public async Task<ActionResult<IEnumerable<ApiOffer>>> PostInquireGetOffers([FromBody] ApiInquire? inquire) {
         if (inquire == null) { return BadRequest(Array.Empty<ApiOffer>()); }
@@ -103,25 +166,6 @@ public class ApiController : ControllerBase {
             }
         }
         return NotFound(); // should not happen if serviceName exists
+    }*/
     }
-
-    /* 
-     * === UNUSED ===
-     * 
-    // GET: <ApiController>/CourierHub/status/q1w2-e3r4-t5y6-u7i8-o9p0
-    [HttpGet("{serviceName}/status/{code}")]
-    public async Task<ActionResult<StatusType?>> GetOrderStatus(string serviceName, string code) {
-        foreach (var webapi in _webApis) {
-            if (webapi.ServiceName == serviceName) {
-                (StatusType? type, int status) = await webapi.GetOrderStatus(code);
-                if (type != null && status >= 200 && status < 300) {
-                    return Ok(type);
-                } else {
-                    return BadRequest(type);
-                }
-            }
-        }
-        return NotFound(null); // should not happen if serviceName exists
-    }
-    */
 }
