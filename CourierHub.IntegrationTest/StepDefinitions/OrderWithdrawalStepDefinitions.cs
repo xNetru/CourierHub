3a382dd6b107995867465a9218348ab3e430b036
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-01-13 23:16:37 +0100

    base for integration tests

---
+++ CourierHub.IntegrationTest/StepDefinitions/OrderWithdrawalStepDefinitions.cs
@@ -0,0 +1,18 @@
+﻿namespace CourierHub.IntegrationTest.StepDefinitions; 
+[Binding]
+public sealed class OrderWithdrawalStepDefinitions {
+    [Given("a user having an order with code (.*)")]
+    public void GivenAUserHavingAnOrderWithCode(string code) {
+        throw new PendingStepException();
+    }
+
+    [When("he requests withrawal at (.*)")]
+    public void HeRequestsWithrawalAt() {
+        throw new PendingStepException();
+    }
+
+    [Then("he receives an anwser (.*)")]
+    public void HeReceivesAnAnwser(int status) {
+        throw new PendingStepException();
+    }
+}
