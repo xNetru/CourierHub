0eca88cd73b627ac6d8e2ad2c5952aa8e5ca5beb
Bakaczor <bartosz.kaczorowski@protonmail.com>
2024-01-14 01:32:20 +0100

    last controller tests and clean up

--- CourierHub.Test/ControllerTest/UserControllerTest.cs
+++ CourierHub.Test/ControllerTest/UserControllerTest.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Moq.EntityFrameworkCore;
 
-namespace CourierHub.Test;
+namespace CourierHub.Test.ControllerTest;
 public class UserControllerTest {
     private readonly UserController _controller;
 
