50c8ee6e42762a5bc7ad1b8f64c7e3212d293c2e
xNetru <kamilbbank@gmail.com>
2024-01-15 21:21:40 +0100

    comments deleted

--- CourierHubWebApi/Services/Contracts/IOrderService.cs
+++ CourierHubWebApi/Services/Contracts/IOrderService.cs
@@ -7,9 +7,6 @@ using CourierHubWebApi.Errors;
 
 namespace CourierHubWebApi.Services.Contracts {
     public interface IOrderService {
-        //ErrorOr<int> CreateOrder(CreateOrderRequest request, int serviceId);
-        //Task<ErrorOr<int>> WithdrawOrder(WithdrawOrderRequest request, int serviceId);
-        //ErrorOr<StatusType> GetOrderStatus(GetOrderStatusRequest request, int serviceId);
         OneOf<int, ApiError> CreateOrder(CreateOrderRequest request, int serviceId);
         Task<OneOf<int, ApiError>> WithdrawOrder(WithdrawOrderRequest request, int serviceId);
         OneOf<StatusType, ApiError> GetOrderStatus(GetOrderStatusRequest request, int serviceId);
--- CourierHubWebApi/Services/PriceCacheService.cs
+++ CourierHubWebApi/Services/PriceCacheService.cs
@@ -16,14 +16,6 @@ namespace CourierHubWebApi.Services {
             }
             return timeStamp;
         }
-        //public ErrorOr<DateTime> SavePrice(string InquireCode, decimal Price) {
-        //    DateTime timeStamp = DateTime.Now.AddMinutes(15);
-        //    if (!_registeredOffers.TryAdd(InquireCode,
-        //                        new OfferData(Price, timeStamp))) {
-        //        return Error.Conflict();
-        //    }
-        //    return timeStamp;
-        //}
 
         public OneOf<decimal, ApiError> GetPrice(string InquireCode, DateTime obtainmentTime)
         {
@@ -45,21 +37,6 @@ namespace CourierHubWebApi.Services {
                 return ApiError.DefaultInternalServerError;
             }
         }
-
-        //public ErrorOr<decimal> GetPrice(string InquireCode, DateTime obtainmentTime) {
-        //    if (!_registeredOffers.TryGetValue(InquireCode, out OfferData? offer)) {
-        //        return Error.NotFound();
-        //    }
-        //    if (obtainmentTime > offer.TimeStamp) {
-        //        return Error.Custom(type: 408, code: "Request Timeout", description: "Offer expired");
-        //    }
-        //    try {
-        //        _registeredOffers.Remove(InquireCode);
-        //        return offer.Price;
-        //    } catch (Exception ex) {
-        //        return Error.Unexpected();
-        //    }
-        //}
         private record OfferData(decimal Price,
                                  DateTime TimeStamp);
     }
