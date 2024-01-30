e280dc92f60d0994870f9b31aa38a8a9c050be42
xNetru <kamilbbank@gmail.com>
2024-01-30 21:01:01 +0100

    Added enum convresion

--- CourierHub/Server/Api/Enums/WeraHubApi/DeliveryStatus.cs
+++ CourierHub/Server/Api/Enums/WeraHubApi/DeliveryStatus.cs
@@ -1,5 +1,8 @@
-﻿namespace CourierHub.Server.Api.Enums.WeraHubApi
+﻿using System.Text.Json.Serialization;
+
+namespace CourierHub.Server.Api.Enums.WeraHubApi
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum DeliveryStatus
     {
         Proccessing, PickedUp, Delivered, CannotDeliver, Canceled
